b5e280127213737cb56e28a86dff81e8f8667cac
Leandro Afonso <leandro.afonso@hotmail.com>
2022-04-24 13:53:00 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const mainTag = window.document.querySelector('#main')
+mainTag.remove()
+
+const newHeader = window.document.createElement("h1")
+newHeader.id = "victory"
+newHeader.textContent = "Leo is the champion"
+document.write(newHeader.innerText)
+
